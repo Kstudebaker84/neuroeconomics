fef1b61918c497e2b17253dd43a85c051f1b3106
kstudebaker84 <kristenstudebaker@yahoo.com>
2018-04-16 16:32:51 -0700

    first commit

---
+++ javascript/canvas.js
@@ -0,0 +1,43 @@
+// window.onload = function () {
+//   var canvas1 = document.getElementById('canvas1');
+//
+//   if (canvas1 && canvas1.getContext) {
+//     var ctx = canvas1.getContext("2d");
+//     if (ctx) {
+//       var myText = "Kristen Studebaker";
+//       ctx.font='bold 50px Manuale';
+//       ctx.fillStyle='#efa83e';
+//       ctx.strokeStyle='black';
+//       ctx.lineWidth=1;
+//       ctx.fillText(myText,20,100);
+//       ctx.strokeText(myText,20,100);
+//
+//       ctx.fillText('HTML5 Canvas',70,150);
+//       ctx.fillText('HTML5 Canvas',120,200);
+//       ctx.fillText('HTML5 Canvas',170,250);
+//
+//       ctx.strokeText('HTML5 Canvas',70,150);
+//       ctx.strokeText('HTML5 Canvas',120,200);
+//       ctx.strokeText('HTML5 Canvas',170,250);
+//
+//     }
+//   }
+// }
+//
+//
+// // var canvas;
+// // var context;
+// // function showText(){canvas=document.getElementById('canvas');
+// // context=canvas.getContext('2d');
+// // var canvas=document.getElementById('canvas').getContext('2d');
+// // context.font='bold 50px Manuale';
+// // context.fillStyle='#D73B22';
+// // context.fillText('HTML5 Canvas',10,100);
+// // context.strokeStyle='#003';
+// // context.lineWidth=3;
+// // context.fillStyle='#C90';
+// // context.textAlign='center';
+// // context.fillText('HTML5 Canvas',320,200);
+// // context.strokeText('HTML5 Canvas',317,203);
+// // }
+// // window.addEventListener('load',showText,false);
